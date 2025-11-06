091b9f3767310a91e80c1389993e41355103ad75
terminator-ger <michael.lechner.trash@t-online.de>
2025-11-06 11:11:02 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='AnkiVocParser',
+      version='0.1',
+      description='OCR and Parser for vocabulary extraction from tables into Anki format',
+      author='terminator-ger',
+      packages=['anki_voc_parser'],
+      )
